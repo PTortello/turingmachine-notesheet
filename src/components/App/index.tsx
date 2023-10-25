1722fcfd0e1a511ac533055c5e5ddcf53b1901b7
Pedro Tortello <ptortello@hotmail.com>
2023-10-25 05:39:31 -0300

    Initial config

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import reactLogo from "assets/react.svg";
+
+function App() {
+  return (
+    <>
+      <p>Hello, World!</p>
+      <p style={{ backgroundColor: "#f0f" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum autem
+        minima ipsum sint. Mollitia minima, laudantium corrupti asperiores
+        veniam iusto cum similique quaerat, magnam in ratione modi. Magnam,
+        expedita id.
+      </p>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </>
+  );
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/turingmachine-notesheet/",
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+      constants: "/src/constants",
+      interfaces: "/src/interfaces",
+      utils: "/src/utils",
+    },
+  },
+});
