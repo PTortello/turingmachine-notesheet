c07771beb8eae3b4cd21c5e36887584cff0eee4d
Pedro Tortello <ptortello@hotmail.com>
2023-10-29 19:13:15 -0300

    CustomButton style update

--- src/components/CustomButton/styles.ts
+++ src/components/CustomButton/styles.ts
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
 
 export const CustomButton = styled.button`
-  height: 36px;
+  height: 40px;
   width: 120px;
   color: #fefefe;
   background-color: #2db563;
   border-color: #2db563;
   border-radius: 4px;
+  font-size: 16px;
   font-weight: bold;
 `;
