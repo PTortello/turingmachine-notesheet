98e2ba3c2062ec9a664906fe65b3b9bc1e972c20
Pedro Tortello <ptortello@hotmail.com>
2023-11-02 16:02:11 -0300

    i18n update

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,14 +1,17 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import de from "./locales/de/translation.json";
 import en from "./locales/en/translation.json";
+import es from "./locales/es/translation.json";
+import fr from "./locales/fr/translation.json";
 import pt from "./locales/pt/translation.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: { en, pt },
+    resources: { de, en, es, fr, pt },
     fallbackLng: "en",
     debug: import.meta.env.DEV,
     interpolation: {
